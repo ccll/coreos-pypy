d9d966371113572719fafd045b5f3bbe6bd1bfbf
ccll <cailei@live.com>
2015-03-05 23:09:20 +0800

    add '-f' to ln

--- install-to-coreos.sh
+++ install-to-coreos.sh
@@ -11,7 +11,7 @@ echo Copying to host...
 cp -rp ${PYPY} /home/host-core/pypy
 
 cd /home/host-core/pypy/bin
-ln -s /lib64/libncurses.so.5.9 libtinfo.so.5
+ln -s -f /lib64/libncurses.so.5.9 libtinfo.so.5
 
 touch /home/host-core/pypy/.ok
 
